33bd976ac6b0ce0ae781ec6e0b7fdde9b32c57a7
sevru <sevruliza@gmail.com>
2026-01-19 17:33:21 +1000

    JavaScript organized

--- JavaScript/intro/scriptjs.js
+++ JavaScript/intro/scriptjs.js
@@ -1,5 +1,6 @@
 ﻿// JavaScript source code
-function Factorial(n) {
+function Factorial(n)
+{
     let f = BigInt(1);
     //document.write(typeof (f));
     //document.write("<pre>");//preformatted учитываются пробелы и знаки переноса
@@ -18,4 +19,24 @@ function Factorial2()
     let source_value = source_input_field.value;
     let factorial_result = document.getElementById("factorial-result");
     factorial_result.innerHTML = Factorial(source_value); 
+}
+
+function Degree2()
+{
+    let source_input_field = document.getElementById("number");
+    let source_input_field2 = document.getElementById("exponent");
+    
+    let source_value = source_input_field.value;
+    let source_value2 = source_input_field2.value;
+    let degree_result = document.getElementById("degree-result");
+    degree_result.innerHTML = Degree(source_value, source_value2);
+}
+
+function Degree(n,e)
+{
+    let number = n;
+    let exponent = e;
+    let f = n ** e;
+
+    return f;
 }
